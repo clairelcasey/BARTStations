182459bf3dc77241715a2425f74e04e8600c071a
Claire Casey <clcasey805@gmail.com>
2020-11-24 12:35:23 -0800

    before refactoring

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ async function getMostRecentUpdateTime() {
       json: "y"
     },
   });
-  console.log(response.data.root);
+  // console.log(response.data.root);
   return {
     date: response.data.root.date,
     time: response.data.root.time
@@ -121,7 +121,7 @@ async function getStationsAndDisplay() {
   console.log('final stations', STATIONS);
   $loading.hide();
   const times = await getMostRecentUpdateTime();
-  console.log('times', times);
+  // console.log('times', times);
   displayTimeAndOptions(times);
   createMap();
 }
@@ -129,6 +129,7 @@ async function getStationsAndDisplay() {
 $(getStationsAndDisplay);
 // $(createMap);
 
+/* Given time from GET request, display time in header */
 
 function displayTimeAndOptions(times) {
   const $time = $("<h3>")
@@ -140,17 +141,29 @@ function displayTimeAndOptions(times) {
   $("#time-option-container").show();
 }
 
-
-
-
-
+/* Initialize d3 elements */
 
 const svg = d3.select('#map')
   .append("svg")
   .attr("height", 640)//height + margin.top + margin.bottom)
-  .attr("width", 640)//width + margin.left + margin.right)
-// .append("g")
-// .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("width", 640);
+
+/* Add zoom feature on svg */
+svg.call(d3.zoom()
+  .extent([[0, 0], [640, 640]])
+  .scaleExtent([1, 8])
+  .on("zoom", zoomed));//width + margin.left + margin.right)
+
+
+/* Initialize 2gs = first "g' will hold map paths; circles will hold BART 
+station circles */
+const g = svg.append("g");
+const circles = svg.append("g")
+
+function zoomed({ transform, d }) {
+  g.attr("transform", transform);
+  circles.attr("transform", transform);
+}
 
 const projection = d3.geoMercator()
   .center([-122.4194, 37.7749])
@@ -160,11 +173,9 @@ const projection = d3.geoMercator()
 const path = d3.geoPath()
   .projection(projection);
 
-const g = svg.append("g");
 
-
-
-/* Wait to create map until the stations have been selected */
+/* Create county outlines and BART circles. 
+* Wait to create map until the stations have been selected */
 function createMap() {
 
   d3.json("california-counties@1.topojson").then(function (topology) {
@@ -180,7 +191,7 @@ function createMap() {
   });
 
   // Create the circles
-  svg.selectAll("BART-circles")
+  circles.selectAll("BART-circles")
     .data(STATIONS)
     .enter()
     .append("circle")
@@ -199,7 +210,8 @@ function createMap() {
       d3.select(this)
         .style('opacity', '1')
         .attr("r", 6);
-      d3.select('#name').text(d.name);
+        // update tooltip
+      d3.select('#name').text(`${d.name} Station`);
       for (let sta of d.etd) {
         d3.select("#etd")
           .append("span")
@@ -210,7 +222,7 @@ function createMap() {
           .attr('class', 'single-etd')
           .text(` train to ${sta.destination} in ${sta.estimate[0].minutes} 
           minutes`);
-
+        // If there is a delay, display the delay in the tooltip in red
         if (sta.estimate[0].delay !== "0") {
           d3.select("#etd")
             .append("span")
@@ -219,6 +231,7 @@ function createMap() {
         }
         d3.select("#etd").append('br');
       }
+      // display tooltip based on the mouseover event
       d3.select('#tooltip')
         .style("opacity", "0.8")
         .style("display", "block")
@@ -230,7 +243,7 @@ function createMap() {
       $("#etd").empty();
       d3.select(this)
         .style('opacity', '0.4')
-        // .style('r', 5);
+      // .style('r', 5);
       d3.select('#tooltip')
         .style("opacity", "0")
         .style("display", "none");
@@ -243,6 +256,10 @@ function createMap() {
 let showDelay = false;
 $("#delayCheck").on("click", updateDelay);
 
+/* If the delay button is clicked, turn on and off strokes around the circles 
+with delays.
+* This function will show any station that has a delay (does not matter if 
+only certain lines are showing). */
 
 function updateDelay() {
   showDelay = (showDelay) ? false : true;
@@ -261,14 +278,17 @@ function updateDelay() {
 
 
 
+/* If a user clicks only to show a specific line, get the color they clicked 
+and update the map to just show that Line. */
 
-
-$("#BART-lines").on("click",".dropdown-item", function(evt) {
+$("#BART-lines").on("click", ".dropdown-item", function (evt) {
   const $button = $(evt.target);
   const color = $button.attr("id");
   updateLine(color);
 })
 
+/* Given a color, only show BART circles that are on that line (tooltip is not 
+  updated so info on all lines will still show) */
 
 function updateLine(color) {
   const lineDataUpdate = filterByColor(color);
@@ -282,53 +302,72 @@ function updateLine(color) {
 }
 
 
+/* Given a color, update the data to have a "show" element that is true if the 
+BART station is on that Line and false if not. 
+* This uses map rather than filter so that the elements aren't actually 
+removed, just not displayed. 
+This is so the tooltip information and mouse clicks don't need to be re-added. 
+*/
 
 function filterByColor(color) {
-  const stationsFiltered = STATIONS.map(function (val) {
-    for (let destination of val.etd) {
-      console.log('destination', destination);
+  const stationsFiltered = STATIONS.map(function (sta) {
+    let newSta = JSON.parse(JSON.stringify(sta));
+    let newEtd = [];
+    let showing = null;
+    for (let destination of newSta.etd) {
+      // console.log('destination', destination);
+      // push the destination if it's on the correct line. Using push because 
+      // there are trains going in both directions that share the same color. 
       if (destination.estimate[0].color === color) {
-        val.show = true;
-        return val;
+        showing = true;
+        newEtd.push(destination);
       }
     }
-    val.show = false;
-    return val;
+    // if showing is still null (for this station, there are no trains 
+    // on the line with this color, then set showing to false)
+    if (showing === null) showing = false;
+    newSta.show = showing;
+    // console.log('newETD', newEtd);
+    newSta.etd = newEtd;
+    // console.log('newSta', newSta);
+    return newSta;
   })
-  console.log(stationsFiltered, 'filtered');
+  console.log(stationsFiltered, 'filtered by color');
   return stationsFiltered
 }
 
 
 $("#resetBtn").on("click", resetMap);
 
+/* Reset the map styles when reset button is clicked. */
+
 function resetMap() {
   d3.selectAll(".BART-circles")
-  .data(STATIONS)
-  .style("fill", "blue")
-  .style("opacity", 0.4)
-  .style("r", 5)
-  .style("display", "block");
+    .data(STATIONS)
+    .style("fill", "blue")
+    .style("opacity", 0.4)
+    .style("r", 5)
+    .style("display", "block");
 }
 
 
+/* If a user clicks on button to display stations by wait, get the 
+color and name they clicked on and run UpdateLineByWait function.*/
 
-
-
-
-
-$("#BART-lines-by-delay").on("click",".dropdown-item", function(evt) {
+$("#BART-lines-by-wait").on("click", ".dropdown-item", function (evt) {
   const $button = $(evt.target);
   const info = $button.attr("id").split('-');
-  console.log('info',info);
+  // console.log('info', info);
 
   const color = info[0];//$button.attr("id");
   const name = info[1];
-  updateLineByDelay(color, name);
+  updateLineByWait(color, name);
 })
 
+/* Given a color and direction, get updated data and update styles/ display for 
+BART circles */
 
-function updateLineByDelay(color, dirName) {
+function updateLineByWait(color, dirName) {
   const lineDataUpdate = filterByColorAndDirection(color, dirName);
   d3.selectAll(".BART-circles")
     .data(lineDataUpdate)
@@ -336,28 +375,29 @@ function updateLineByDelay(color, dirName) {
     .style("display", function (d) {
       return (d.show) ? "block" : "none"
     })
-    .style("r", function(d) {
+    .style("r", function (d) {
       if (d.etd[0].estimate[0].minutes === "Leaving") return 2;
       // console.log(d.etd[0].estimate[0].minutes,"d");
       return d.etd[0].estimate[0].minutes;
     });
 }
 
-
+/* Given a color and direction, filter data based on parameters. */
 
 function filterByColorAndDirection(color, dirName) {
   const stationsFiltered = STATIONS.map(function (val) {
-    for (let elem of val.etd) {
-      console.log('elem', elem);
-      if (elem.estimate[0].color === color && elem.destination === dirName) {
-        val.show = true;
-        val.etd = [elem];
-        return val;
+    let newVal = JSON.parse(JSON.stringify(val));
+    for (let elem of newVal.etd) {
+      // console.log('elem', elem);
+      if (elem.estimate[0].color === color && elem.abbreviation === dirName) {
+        newVal.show = true;
+        newVal.etd = [elem];
+        return newVal;
       }
     }
-    val.show = false;
-    return val;
+    newVal.show = false;
+    return newVal;
   })
-  console.log(stationsFiltered, 'filtered');
+  console.log(stationsFiltered, 'filtered by color and direction');
   return stationsFiltered
 }
